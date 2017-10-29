adaf9c5277161580ebff37c2910152e64a7db359
phincallahan <phincallahan14@gmail.com>
2017-10-29 15:21:32 +0100

    refactored python solution 37

--- python/problem37.py
+++ python/problem37.py
@@ -1,42 +1,33 @@
-from pyprimesieve import primes as get_primes
+from lib.performance import do_cprofile
+from lib.primes import prime_sieve, isPrime
 
-PRIMES = get_primes(10 ** 4)
-def isPrime(x):
-    for prime in PRIMES:
-        if x % prime == 0:
-            return False
-        elif prime * prime > x:
-            return True
+@do_cprofile
+def solution():
+    primes = prime_sieve(10 ** 4)
 
-    prime = PRIMES[-1]
-    while prime * prime < x:
-        prime += 1
-        if x % prime == 0:
-            return False
+    rq = set([2, 3, 7, 5])
+    lq = set([2, 3, 7, 5])
+    trunc = set()
 
-    return True
+    while len(trunc) < 11:
+        r_temp = set()
+        l_temp = set()
 
-rq = set([2, 3, 7, 5])
-lq = set([2, 3, 7, 5])
-trunc = set()
+        for s in [1, 2, 3, 5, 7, 9]:
+            for rt in rq:
+                new_rt = (10 ** len(str(rt))) * s + rt
+                if isPrime(new_rt):
+                    r_temp.add(new_rt)
 
-while len(trunc) < 11:
-    r_temp = set()
-    l_temp = set()
+            for lt in lq:
+                new_lt = 10 * lt + s
+                if isPrime(new_lt):
+                    l_temp.add(new_lt)
+        
+        rq = r_temp
+        lq = l_temp
+        trunc = trunc | (rq & lq)
 
-    for s in [1, 2, 3, 5, 7, 9]:
-        for rt in rq:
-            new_rt = (10 ** len(str(rt))) * s + rt
-            if isPrime(new_rt):
-                r_temp.add(new_rt)
+    return sum(trunc)
 
-        for lt in lq:
-            new_lt = 10 * lt + s
-            if isPrime(new_lt):
-                l_temp.add(new_lt)
-    
-    rq = r_temp
-    lq = l_temp
-    trunc = trunc | (rq & lq)
-
-print(sum(trunc))
+solution()
