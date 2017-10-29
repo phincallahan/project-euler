f04065f967b28d041bc76fa0ebce40c117788e7d
phincallahan <phincallahan14@gmail.com>
2017-10-29 16:22:55 +0100

    solved problem 39 in python

--- python/problem39.py
+++ python/problem39.py
@@ -1,33 +1,14 @@
-import numpy as np
-from collections import defaultdict
+from lib.pythagorean import primitive_triplets
+from lib.performance import do_cprofile
+from collections import Counter
 
-def isPythagoreanTriplet(x):
-    return x[0] * x[0] + x[1] * x[1] - x[2] * x[2] == 0
+@do_cprofile
+def solution():
+    c = Counter()
+    triplet_gen = primitive_triplets(lambda t: sum(t) <= 1000)
+    for t in triplet_gen:
+        c.update(sum(t) * k for k in range(1, 1000 // sum(t) + 1))
 
-def pythag_triples():
-    A = np.array(
-        [[ 1, -2, 2], [ 2, -1, 2], [ 2, -2, 3],
-         [ 1,  2, 2], [ 2,  1, 2], [ 2,  2, 3],
-         [-1,  2, 2], [-2,  1, 2], [-2,  2, 3]]
-    )
+    return c.most_common(1)[0][0]
 
-    queue = [np.array([3, 4, 5])]
-    while len(queue):
-        x = queue.pop()
-        if sum(x) < 1000:
-            y = np.copy(x)
-            while sum(y) < 1000:
-                if isPythagoreanTriplet(y):
-                    yield y
-                y += x
-
-            queue.extend(A.dot(x).reshape((3,3)))
-
-count = defaultdict(int)
-m = (0, 0)
-for x in pythag_triples():
-    count[sum(x)] += 1
-    if count[sum(x)] > m[0]:
-        m = (count[sum(x)], sum(x))
-
-print(m)
+solution()
