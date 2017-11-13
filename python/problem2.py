f50c9679c088718e704d8a9fa61d0f845f06d2d5
phincallahan <phincallahan14@gmail.com>
2017-11-13 14:01:36 +0100

    solved problem 2 in python

---
+++ python/problem2.py
@@ -0,0 +1,14 @@
+from itertools import takewhile
+
+def fib():
+    prev, curr = 1, 1
+    while True:
+        yield curr
+        t, curr = curr, curr + prev
+        prev = t
+
+def solution():
+    bounded_fib = takewhile(lambda x: x < 4 * 10 ** 6, fib()) 
+    return sum(x for x in bounded_fib if x % 2 == 0)
+
+print(solution())
