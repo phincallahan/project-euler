87b353a7cca1a5b7e0b2224de92d6845d730c947
phincallahan <phincallahan14@gmail.com>
2017-11-13 14:02:18 +0100

    solved problem 4 in python

---
+++ python/problem4.py
@@ -0,0 +1,11 @@
+from lib.palindrome import isPalindrome
+
+def solution():
+    m = 10 * 8
+    for i in range(999, 99, -1):
+        if i ** 2 < m: return m
+        for j in range(i, 99, -1):
+            if isPalindrome(j * i) and i * j > m:
+                m = i * j 
+
+print(solution())
