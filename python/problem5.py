ed7b6173ea0be2d338e7b81e60df01733ee452ab
phincallahan <phincallahan14@gmail.com>
2017-11-17 21:40:56 +0100

    refactored

--- python/problem5.py
+++ python/problem5.py
@@ -1,10 +1,11 @@
-from lib.factorization import factor
-from lib.primes import prime_sieve
+from lib.factorization import factor_method
 
 def solution():
-    primes = [2, 3, 5, 7, 11, 13, 17, 19]
-    f = factor(1, primes)
-    for n in range(2, 21):
-        f |= factor(n, primes)
+    factor = factor_method([2, 3, 5, 7, 11, 13, 17, 19])
+    factorization = factor(1)
+    for num in range(2, 21):
+        factorization |= factor(num)
 
-    return int(f)
+    return int(factorization)
+
+print(solution())
