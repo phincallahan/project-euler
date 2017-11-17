1ed2dbe31343a7fc8e9409473d7c1657c885dc10
phincallahan <phincallahan14@gmail.com>
2017-11-17 21:49:09 +0100

    refactored

--- python/problem50.py
+++ python/problem50.py
@@ -1,10 +1,8 @@
-from lib.performance import do_cprofile
-from lib.primes import prime_sieve, isPrime
+from lib.primes import prime_sieve, miller_rabin
 from itertools import count, accumulate
 
-@do_cprofile
 def solution():
-    primes = prime_sieve(10 ** 4)
+    primes = list(prime_sieve(10 ** 6))
 
     N = 10 ** 6
     for max_i, p_sum in enumerate(accumulate(primes)):
@@ -15,10 +13,7 @@ def solution():
         for j in count():
             if p_sum >= N:
                 break
-            if isPrime(p_sum):
+            if miller_rabin(p_sum):
                 return p_sum
             else:
                 p_sum = p_sum + primes[i + j] - primes[j]
-
-
-solution()
